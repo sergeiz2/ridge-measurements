479602640eaa699a994a00f0cd79e4a4bd381aea
Sergei Zvenigorodsky <serge.zvenig@gmail.com>
2022-01-03 10:47:40 -0600

    Added mysql connection

--- parse.py
+++ parse.py
@@ -8,40 +8,67 @@ import re
 
 dir = os.getcwd() + '\CSVs'
 
+db = mysql.connect(
+    host = "localhost",
+    user = "editekk",
+    passwd = "editekk",
+    database = "RidgeMeasurements"
+)
+cursor = db.cursor()
+
+# print(db)
+# cursor.execute("SHOW DATABASES")
+# print(cursor.fetchall())
+
 def find_radial(df):
 
     try:
         r3_ind = df.iloc[:, 0].tolist().index('R3')
+    
     except ValueError:
-        r3_ind = -1
+        try:
+            r3_ind = df.iloc[:, 0].tolist().index('r3')
+        except ValueError:
+            r3_ind = -1
+    
     try:
         r2_ind = df.iloc[:, 0].tolist().index('R2')
+    
     except ValueError:
-        r2_ind = -1
+        try:
+            r2_ind = df.iloc[:, 0].tolist().index('r2')
+        except ValueError:
+            r2_ind = -1
+    
     try:
         r1_ind = df.iloc[:, 0].tolist().index('R1')
+    
     except ValueError:
-        r1_ind = -1
+        try:
+            r1_ind = df.iloc[:, 0].tolist().index('r1')
+        except ValueError:
+            r1_ind = -1
 
             
 
     # 'R3'.casefold() in (str(val).casefold() for val in df.iloc[:, 0].tolist()))
 
 
-def clean_csv(pathstr=None):
+def find_sample_names(pathstr=None):
     sample_name = None
 
     dataframe = pd.read_csv(pathstr)
     
     try: 
         sample_name = re.search('DIS_A([0-9][0-9])_([0-9][0-9])', pathstr).group()
-        print('Sample ' + sample_name)
+        # print('Sample ' + sample_name)
     except AttributeError:
         pass
 
     find_radial(dataframe)
 
-for file in os.scandir(dir):
-    if 'DIS_A1' in file.path:
-        clean_csv(file.path)
+def clean_csv():
+    for file in os.scandir(dir):
+        if 'DIS_A1' in file.path:
+            find_sample_names(file.path)
 
