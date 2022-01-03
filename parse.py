676662261058ce40fc267f0eef018c7bab2edb73
Sergei Zvenigorodsky <serge.zvenig@gmail.com>
2022-01-03 13:29:46 -0600

    got index tracker for radial locations working

--- parse.py
+++ parse.py
@@ -20,11 +20,9 @@ cursor = db.cursor()
 # cursor.execute("SHOW DATABASES")
 # print(cursor.fetchall())
 
-def find_radial(df):
+def find_where_radial(df):
 
-    global r3_ind
-    global r2_ind
-    global r1_ind
+    global r_loc_dict 
 
     try:
         r3_ind = df.iloc[:, 0].tolist().index('R3')
@@ -53,26 +51,58 @@ def find_radial(df):
         except ValueError:
             r1_ind = -1
 
-            
+    r_loc_dict = {
+        "r1_index": r1_ind,
+        "r2_index": r2_ind,
+        "r3_index": r3_ind
+    }
+
+    return r_loc_dict
 
     # 'R3'.casefold() in (str(val).casefold() for val in df.iloc[:, 0].tolist()))
 
 
-def find_sample_names(pathstr=None):
-    sample_name = None
-
-    dataframe = pd.read_csv(pathstr)
+def find_sample_name(pathstr=None):
     
+    global sample_name
+
     try: 
         sample_name = re.search('DIS_A([0-9][0-9])_([0-9][0-9])', pathstr).group()
         # print('Sample ' + sample_name)
     except AttributeError:
         pass
 
-    find_radial(dataframe)
+    return sample_name
 
-def clean_csv():
+def populate_index_tracker(name, radial_locs):
+    query = "INSERT INTO index_tracker (sample_id, r1_ind, r2_ind, r3_ind) VALUES (%s, %s, %s, %s)"
+    values = (name, *radial_locs)
+
+    # print(values)
+
+    cursor.execute(query, values)
+    db.commit()
+
+
+
+
+def clean_csvs():
+    cursor.execute("DROP TABLE index_tracker")
+    cursor.execute("CREATE TABLE index_tracker (id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, sample_id VARCHAR(15), r1_ind SMALLINT, r2_ind SMALLINT,  r3_ind SMALLINT)")
+
     for file in os.scandir(dir):
-        if 'DIS_A1' in file.path:
-            find_sample_names(file.path)
+        pth = file.path
+        if 'DIS_A1' in pth:
+            find_sample_name(pth)
+            
+            data = pd.read_csv(pth)
+        
+
+        find_where_radial(data)
+
+        populate_index_tracker(sample_name, r_loc_dict.values())
+
+    # a = cursor.fetchall()
+    
+clean_csvs()
 
