eff620eb1bccaf5b42d33f6889f7c7be0cafe31c
Sergei Zvenigorodsky <serge.zvenig@gmail.com>
2022-01-05 19:54:00 -0600

    added parse_csvs.py and setup.py files

---
+++ parse_csvs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import numpy as np
+import mysql.connector as mysql
+import os
+import re
+import click
+from pandas.core.frame import DataFrame
+from pandas.core.indexes.base import Index
+from pandas.core.reshape.merge import merge
+
+from pandas.core.series import Series
+
+def main():
+    '''A CSV parsing CLI for Editekk's Ridge Measurements'''
+    
+    print(
+    "This CLI will help process and clean the RidgeMeasurement CSVs."
+    "For help, use the --help (-h) flag.")
+    pass
+# TODO: set up help
+
+def 
