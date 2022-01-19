e65806ddb219e97c02514b9f00cde624c57e43e4
Sergei Zvenigorodsky <serge.zvenig@gmail.com>
2022-01-19 16:04:05 -0600

    Fixed ER diagram and *.sql syntax

--- Editekk_ridge_meas.sql
+++ Editekk_ridge_meas.sql
@@ -2,16 +2,14 @@ CREATE TABLE "irrad_batch" (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "name" varchar,
   "date" timestamptz NOT NULL DEFAULT (now()),
-  "tgt_fluence" double precision NOT NULL,
-  "avg_fluence" double precision NOT NULL,
   "angle" real NOT NULL,
-  "base_press" double precision NOT NULL,
-  "op_press" double precision NOT NULL,
-  "bm_volt" real NOT NULL,
+  "tgt_fluence" "double precision" NOT NULL,
+  "duration" real NOT NULL,
+  "base_press" "double precision" NOT NULL,
   "start_curr" real NOT NULL,
-  "start_flux" GENERATED ALWAYS AS (end_curr * cos(angle) * 5006.1731) STORED,
-  "end_curr" real NOT NULL,
-  "time" real NOT NULL,
+  "start_flux" "generated always as (end_curr * cos(angle) * 5006.1731) stored" NOT NULL,
+  "op_press" "double precision" NOT NULL,
+  "bm_volt" real NOT NULL,
   "cath_curr" real NOT NULL,
   "disch_curr" real NOT NULL,
   "disch_volt" real NOT NULL,
@@ -20,6 +18,8 @@ CREATE TABLE "irrad_batch" (
   "acc_volt" real NOT NULL,
   "nutr_emiss" real NOT NULL DEFAULT 0,
   "fil_curr" real NOT NULL DEFAULT 0,
+  "end_curr" real NOT NULL,
+  "avg_fluence" "double precision" NOT NULL,
   "misc" jsonb
 );
 
@@ -69,25 +69,21 @@ COMMENT ON COLUMN "irrad_batch"."name" IS 'Name of this batch';
 
 COMMENT ON COLUMN "irrad_batch"."date" IS 'When this batch was irradiated';
 
-COMMENT ON COLUMN "irrad_batch"."tgt_fluence" IS 'Target fluence (tera-ions per cm squared, 1E+12)';
-
-COMMENT ON COLUMN "irrad_batch"."avg_fluence" IS 'Experimental fluence, calculated from average current and angle (tera-ions per cm squared, 1E+12)';
-
 COMMENT ON COLUMN "irrad_batch"."angle" IS 'radians';
 
+COMMENT ON COLUMN "irrad_batch"."tgt_fluence" IS 'Target fluence (tera-ions per cm squared, 1E+12)';
+
+COMMENT ON COLUMN "irrad_batch"."duration" IS 'Irradiation time, calculated from target fluence (sec)';
+
 COMMENT ON COLUMN "irrad_batch"."base_press" IS 'Lowest chamber pressure achieved before turning on DIS gas flow (Torr)';
-
-COMMENT ON COLUMN "irrad_batch"."op_press" IS 'Operating pressure, after gas flow and ion source are turned on (Torr)';
-
-COMMENT ON COLUMN "irrad_batch"."bm_volt" IS 'Beam voltage and electron energy are the same value. (V, eV, respectively)';
 
 COMMENT ON COLUMN "irrad_batch"."start_curr" IS 'Ion beam curent measured by current plate before irradiation (mA)';
 
 COMMENT ON COLUMN "irrad_batch"."start_flux" IS 'Flux calculated from start_curr (tera-ions per cm squared per second, 1E+12)';
 
-COMMENT ON COLUMN "irrad_batch"."end_curr" IS 'Current measured after irradiation (mA)';
+COMMENT ON COLUMN "irrad_batch"."op_press" IS 'Operating pressure, after gas flow and ion source are turned on (Torr)';
 
-COMMENT ON COLUMN "irrad_batch"."time" IS 'Irradiation time, calculated from target fluence (sec)';
+COMMENT ON COLUMN "irrad_batch"."bm_volt" IS 'Beam voltage and electron energy are the same value. (V, eV, respectively)';
 
 COMMENT ON COLUMN "irrad_batch"."cath_curr" IS 'Cathode filament current on ion source (A)';
 
@@ -104,5 +100,9 @@ COMMENT ON COLUMN "irrad_batch"."acc_volt" IS 'Accelerator voltage (V)';
 COMMENT ON COLUMN "irrad_batch"."nutr_emiss" IS 'Neutralizer emission (mA)';
 
 COMMENT ON COLUMN "irrad_batch"."fil_curr" IS 'Filament current (mA)';
+
+COMMENT ON COLUMN "irrad_batch"."end_curr" IS 'Current measured after irradiation (mA)';
+
+COMMENT ON COLUMN "irrad_batch"."avg_fluence" IS 'Experimental fluence, calculated from average current and angle (tera-ions per cm squared, 1E+12)';
 
 COMMENT ON COLUMN "irrad_batch"."misc" IS 'Notes and miscellaneous other values';
